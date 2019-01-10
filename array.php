2aee1442d4e7788d985ccdc261c3d2d0d1c9a926
David Weston Webb <westwebb@gmail.com>
2019-01-10 10:10:32 -0500

    Create array.php

---
+++ array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$NumberList =
+array (267, 8765, 345, '5345', 365, '<h1>Hello</h1>);
+
+//print_r($NumberList);
+
+echo $NumberList[1];
+echo $NumberList[5];
+echo $NumberList[0];
+
+?>
+
+</body>
